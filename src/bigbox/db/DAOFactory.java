e622092241f3d03e06c894fb2544489c7be15366
ShawnHumphries <sahump1@gmail.com>
2017-05-08 17:29:00 -0400

    Updated DAOFactory to add new DivisionDAO and StoreSalesDAO objects

--- src/bigbox/db/DAOFactory.java
+++ src/bigbox/db/DAOFactory.java
@@ -1,10 +1,29 @@
 package bigbox.db;
 
+import bigbox.db.division.DivisionDAO;
+import bigbox.db.division.DivisionDB;
+import bigbox.db.store.StoreDAO;
+import bigbox.db.store.StoreDB;
+import bigbox.db.storesales.StoreSalesDAO;
+import bigbox.db.storesales.StoreSalesDB;
+
 public class DAOFactory {
 
+	public static DivisionDAO getDivisionDAO()
+	{
+		DivisionDAO dDAO = new DivisionDB();
+		return dDAO;
+	}
+	
 	public static StoreDAO getStoreDAO()
 	{
 		StoreDAO sDAO = new StoreDB();
 		return sDAO;
+	}
+	
+	public static StoreSalesDAO getStoreSalesDAO()
+	{
+		StoreSalesDAO ssDAO = new StoreSalesDB();
+		return ssDAO;
 	}
 }
